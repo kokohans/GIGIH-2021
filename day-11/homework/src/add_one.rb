beb0d0a4bcc0e690d0dd69344f32f357b4f5ea24
kokohans <hw.wiranata@gmail.com>
2021-07-28 22:30:16 +0700

    create a loop through array value

--- day-11/homework/src/add_one.rb
+++ day-11/homework/src/add_one.rb
@@ -7,15 +7,11 @@ class AddOne
         curr_index = 0
 
         input[curr_index] += INCREMENT_VAL
-        if input[curr_index] > 9
-            input[curr_index] %=10
+
+        while input[curr_index] > 9 && curr_index <= input.length
+            input[curr_index] %= 10
             curr_index+=1
             input[curr_index] += INCREMENT_VAL
-            if input[curr_index] > 9
-                input[curr_index] %= 10
-                curr_index+=1
-                input[curr_index] += INCREMENT_VAL
-            end
         end
 
         input.reverse
