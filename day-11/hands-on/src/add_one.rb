3879ec79c5eef237b7c46daea4e6e36583789bfb
kokohans <hw.wiranata@gmail.com>
2021-07-28 20:45:55 +0700

    day-11 without homework

--- day-11/hands-on/src/add_one.rb
+++ day-11/hands-on/src/add_one.rb
@@ -12,18 +12,32 @@ class AddOne
         #     arr[arr.length-2] += 1
         # end
 
-
-        arr[-1] += 1
+        #arr.reverse
+        helper = Array.new(arr[0..-2])
+        res = Array.new
+        arr[-1]+=1
+        carry = 0
         if arr[-1] >= 10
+            carry
             arr[-1] = 0
-            arr[arr.length-2] += 1
+                helper.reverse
+                helper.each do | x |
+                    if carry == 1
+                        x+=1
+                        carry = 0
+                    end
+                    if x >= 10
+                        carry = 1
+                        x = 0
+                    end
+                    res << x
+                end
         end
-        if arr[0] >= 10
-            arr.unshift(1)
-        end
-        if arr[1] >= 10
-            arr[1] = 0
-        end
-        arr
+
+        res << arr[-1]
     end
 end
+
+add_one = AddOne.new
+
+puts add_one.add([1,2,9])
